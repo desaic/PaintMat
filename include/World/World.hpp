8902837ef12b873f9077c7237968a93a3c78fe9a
Desai Chen <desaicwtf@gmail.com>
2013-08-20 11:47:18 -0400

    init

---
+++ include/World/World.hpp
@@ -0,0 +1,7 @@
+#ifndef WORLD_HPP
+#define WORLD_HPP
+class World
+{
+
+};
+#endif
---
+++ src/lib/World.cpp
@@ -0,0 +1,10 @@
+/*
+ * World.cpp
+ *
+ *  Created on: Aug 20, 2013
+ *      Author: desaic
+ */
+#include "World/World.hpp"
+
+
+
