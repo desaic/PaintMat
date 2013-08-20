f906ca3020d4d5d8797ecf8f37f2792834fbbcd2
Desai Chen <desaicwtf@gmail.com>
2013-08-20 12:02:27 -0400

    init

---
+++ extern/include/vecmath.h
@@ -0,0 +1,12 @@
+#ifndef VECMATH_H
+#define VECMATH_H
+
+#include "vecmath/Matrix2f.h"
+#include "vecmath/Matrix3f.h"
+#include "vecmath/Matrix4f.h"
+#include "vecmath/Quat4f.h"
+#include "vecmath/Vector2f.h"
+#include "vecmath/Vector3f.h"
+#include "vecmath/Vector4f.h"
+
+#endif // VECMATH_H
